3de47a96df40e187d192d3118221e14c8a6eca8f
naijaping <abbeyprince02@gmail.com>
2013-09-02 10:18:44 +0100

    Update list-newest-members-for-bp.php

--- list-newest-members-for-bp.php
+++ list-newest-members-for-bp.php
@@ -1,7 +1,5 @@
 <?php
-/**
- *  Make sure BuddyPress is loaded
- */
+/*************Make sure BuddyPress is loaded ********************************/
 if ( !function_exists( 'bp_core_install' ) ) {
 	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
 	if ( is_plugin_active( 'buddypress/bp-loader.php' ) )
@@ -9,20 +7,38 @@ if ( !function_exists( 'bp_core_install' ) ) {
 	else
 		return;
 }
-/***************************** Start **************************************/
+/*******************************************************************/
 
-	function bp_list_newest_member_widget ($args) {
+
+/**
+ * bp_list_newest_members_register_widgets
+ * register widget.
+ */
+
+function bp_list_newest_members_register_widgets() {
+	add_action('widgets_init', create_function('', 'return register_widget("Bp_List_Newest_Members_Widget");') );
+}
+add_action( 'plugins_loaded','bp_list_newest_members_register_widgets' );
+
+class Bp_List_Newest_Members_Widget extends WP_Widget {
+	
+	function bp_list_newest_members_widget() {
+		$widget_ops = array('classname' => 'widget_list_newest_members', 'description' => __( "List Photos And Names Of The Newest Registered Members", "bp-list-newest-members") );
+		parent::WP_Widget( false, $name = __('Newest Members','bp-list-newest-members'), $widget_ops);
+	}
+
+function widget($args, $instance) {
 		global $bp;
 
-	         extract( $args );
-                 echo $before_widget;
-		 echo $before_title
-		   . 'Recent Member'
+		extract( $args );
+
+		echo $before_widget;
+		echo $before_title
+		   . $widget_name
 		   . $after_title; ?>
-		  
-         <?php if ( bp_has_members( 'type=newest&max=5' ) ) : ?>
-		
-	 <ul id="members-list" class="item-list">
+
+<?php if ( bp_has_members( 'user_id=0&type=newest&max='. $instance['max_num'] . '&populate_extras=0' ) ) : ?>
+<ul id="members-list" class="item-list">
 	 <?php while ( bp_members() ) : bp_the_member(); ?>
          <li style="padding: 5px 0 29px 0">
          <div class="item-avatar">
@@ -37,7 +53,7 @@ if ( !function_exists( 'bp_core_install' ) ) {
 	<?php else: ?>
 
 	<div class="widget-error">
-	<?php _e( 'Sorry no newest member ', 'buddypress' ) ?>
+	<?php _e( 'Sorry no newest member ', 'bp-list-newest-members' ) ?>
 	</ul></div>
 
 	<?php endif; ?>
@@ -47,7 +63,21 @@ if ( !function_exists( 'bp_core_install' ) ) {
 <?php
 	}
 
-/*********************************END***************************************/
+function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['max_num'] = strip_tags( $new_instance['max_num'] );
 
-register_sidebar_widget('Recent Member','bp_list_newest_member_widget');
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'max_num' => 5 ) );
+		$max_num = strip_tags( $instance['max_num'] );
+		?>
+		
+		<p><label for="bp-list-newest-members-widget-max-num"><?php _e( 'Max Number of Members:','bp-list-newest-members' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_num' ); ?>" name="<?php echo $this->get_field_name( 'max_num' ); ?>" type="text" value="<?php echo attribute_escape( $max_num ); ?>" style="width: 30%" /></label></p>
+		
+	<?php
+	}
+}
 ?>
