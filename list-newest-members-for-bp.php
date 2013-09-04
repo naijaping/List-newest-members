ffd09fe639db377fdeeae90244096c67792ce7ac
naijaping <abbeyprince02@gmail.com>
2013-09-04 10:25:02 +0100

    Update list-newest-members-for-bp.php
    You can now change the widget title to what ever you like

--- list-newest-members-for-bp.php
+++ list-newest-members-for-bp.php
@@ -3,7 +3,7 @@
 Plugin Name: BP List Newest members
 Plugin URI: http://cityflavourmagazine.com
 Description: Show photos and names of the newest members in the widget area 
-Version: 1.5.1
+Version: 1.5.4
 Requires at least: WordPress 3.0 / BuddyPress 1.3
 Tested up to: WordPress 3.6  / BuddyPress 1.8 
 License: GNU/GPL 2
@@ -39,7 +39,9 @@ class Bp_List_Newest_Members_Widget extends WP_Widget {
 	
 	function bp_list_newest_members_widget() {
 		$widget_ops = array('classname' => 'widget_list_newest_members', 'description' => __( "List Photos And Names Of The Newest Registered Members", "bp-list-newest-members") );
-		parent::WP_Widget( false, $name = __('Newest Members','bp-list-newest-members'), $widget_ops);
+		
+  
+  parent::WP_Widget( false, __('Newest Members','bp-list-newest-members'), $widget_ops);   
 	}
 
 function widget($args, $instance) {
@@ -49,10 +51,10 @@ function widget($args, $instance) {
 
 		echo $before_widget;
 		echo $before_title
-		   . $widget_name
-		   . $after_title; ?>
+		   .$instance['title']
+                   . $after_title; ?>
 
-<?php if ( bp_has_members( 'user_id=0&type=newest&max='. $instance['max_num'] . '&populate_extras=0' ) ) : ?>
+<?php if ( bp_has_members( 'user_id=0&type=newest&max='. $instance['max_num'] .'&populate_extras=0' ) ) : ?>
 <ul id="members-list" class="item-list">
 	 <?php while ( bp_members() ) : bp_the_member(); ?>
          <li>
@@ -80,17 +82,25 @@ function widget($args, $instance) {
 
 function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['max_num'] = strip_tags( $new_instance['max_num'] );
+		$instance['title'] = strip_tags( $new_instance['title'] );
+                $instance['max_num'] = strip_tags( $new_instance['max_num'] );
 
 		return $instance;
 	}
 
 	function form( $instance ) {
 		$instance = wp_parse_args( (array) $instance, array( 'max_num' => 5 ) );
-		$max_num = strip_tags( $instance['max_num'] );
+		$title = strip_tags( $instance['title'] );
+                $max_num = strip_tags( $instance['max_num'] );
 		?>
 		
-		<p><label for="bp-list-newest-members-widget-max-num"><?php _e( 'Max Number of Members:','bp-list-newest-members' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_num' ); ?>" name="<?php echo $this->get_field_name( 'max_num' ); ?>" type="text" value="<?php echo attribute_escape( $max_num ); ?>" style="width: 30%" /></label></p>
+
+<p><label for="bp-list-newest-members-widget-title"><?php _e( 'Title' , 'bp-list-newest-members'); ?>
+ <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" class="widefat" value="<?php echo esc_attr( $title ); ?>"style="width: 100%"/></label></p>
+
+<p><label for="bp-list-newest-members-widget-max-num"><?php _e( 'Max Number of Members:','bp-list-newest-members' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_num' ); ?>" name="<?php echo $this->get_field_name( 'max_num' ); ?>" type="text" value="<?php echo attribute_escape( $max_num ); ?>" style="width: 30%" /></label></p>
+
+
 		
 	<?php
 	}
